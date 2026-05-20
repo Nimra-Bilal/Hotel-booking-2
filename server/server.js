d903b611d8613da1874ed981599fca48f2a3d76c
Nimra Bilal <yoyo123000001@gmail.com>
2026-05-21 00:30:29 +0500

    app.listen silented

--- server/server.js
+++ server/server.js
@@ -46,9 +46,11 @@ app.use("/api/booking", bookingRouter);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+export default app;
 
 
 // app.use(cookieParser());
